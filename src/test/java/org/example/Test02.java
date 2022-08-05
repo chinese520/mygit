d0c71317237a1d5f58fa998577604833347b21d4
cwj <1965244994@qq.com>
2022-08-05 21:24:02 +0800

    gittest操作test01

--- src/test/java/org/example/Test02.java
+++ src/test/java/org/example/Test02.java
@@ -4,6 +4,5 @@ public class Test02 {
     public static void main(String[] args) {
         System.out.println("test02...");
         System.out.println("gittest操作...");
-        System.out.println("mygit操作...");
     }
 }
