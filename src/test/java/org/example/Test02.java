c97c75e1b2ac06132858e1cfa7c1b59b9e736fd3
cwj <1965244994@qq.com>
2022-08-05 21:26:46 +0800

    gittest操作test02

--- src/test/java/org/example/Test02.java
+++ src/test/java/org/example/Test02.java
@@ -3,6 +3,6 @@ package org.example;
 public class Test02 {
     public static void main(String[] args) {
         System.out.println("test02...");
-        System.out.println("gittest操作...");
+        System.out.println("gittest第二操作...");
     }
 }
