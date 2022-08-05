37a414ce5c6d3e659993e97491501bab40aa88ce
cwj <1965244994@qq.com>
2022-08-05 16:52:24 +0800

    test01修改拉取测试

--- src/test/java/org/example/Test01.java
+++ src/test/java/org/example/Test01.java
@@ -4,6 +4,5 @@ public class Test01 {
     public static void main(String[] args) {
         System.out.println("test01...");
         System.out.println("test02...");
-        System.out.println("test03...");
     }
 }
