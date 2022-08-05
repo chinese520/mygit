877c38a115c97e9f56ae63c07bb5c965707d9c05
cwj <1965244994@qq.com>
2022-08-05 21:19:08 +0800

    gittest客户端操作test02

---
+++ src/test/java/org/example/Test02.java
@@ -0,0 +1,8 @@
+package org.example;
+
+public class Test02 {
+    public static void main(String[] args) {
+        System.out.println("test02...");
+        System.out.println("gittest操作...");
+    }
+}
